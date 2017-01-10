554b4e99266c901137e850a5bdd72f690bdace27
RS-Crawford <sam.crawford@qubit.com>
2017-01-10 14:35:37 -0800

    Added PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	apps: [{
+    name: 'tutorial-2',
+    scripts: 'index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-35-166-78-164.us-west-2.compute.amazonaws.com',
+      key: '/users/samcrawford/Downloads/TUTORIAL.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:RS-Crawford/express-EC2_tutorial.git',
+      path: 'home/ubuntu/express-EC2_tutorial',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosysm.config.js'
+    }
+  }
+}
