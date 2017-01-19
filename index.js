caf91f5ed2d730d40415a20d0f1f24c4c6acc0b4
RS-Crawford <sam.crawford@qubit.com>
2017-01-19 14:17:54 -0800

    Server change

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('public'))
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Server running on port 3000!')
 })
